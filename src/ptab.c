06f0136352f28a23d0ade8455bebd5a6bb82f3b9
Marshel Helsper <marsol0x@gmail.com>
2013-10-13 00:07:49 -0400

    Created prefix-tables general data sctructure. For the telnet chat server, this will primarily be used for commands

--- src/ptab.c
+++ src/ptab.c
@@ -2,24 +2,42 @@
 #include <string.h>
 
 #include "ptab.h"
-#include "user.h"
 
-ptab * ptab_init(ptab *pt, char *p)
+ptab * ptab_init()
 {
-    // Find where this prefix should go
-    ptab *t = pt;
-    for (size_t i = 0; i < strlen(p); i++) {
-        ptab *tmp = t->prefix['a' - p[i]];
-        if (tmp == NULL) {
-            tmp = (ptab *) malloc(sizeof(ptab));
-        } 
-        t = tmp;
-    }
-
-    return t;
+    ptab *pt = (ptab *) malloc(sizeof(ptab));
+    pt->data = NULL;
+    for (int i = 0; i < PTAB_MAX_PREFIX; i++)
+        pt->prefix[i] = NULL;
+    return pt;
 }
 
-int ptab_add(ptab *pt, char *prefix, void (* command)(t_user *u, char *data))
+ptab * ptab_add(ptab *pt, char *prefix, void *data)
 {
-    return 0;
+    ptab **p = &pt;
+    for (size_t i = 0; i < strlen(prefix); i++) {
+        p = &((*p)->prefix[prefix[i] - 'a']);
+        if (*p == NULL) {
+            *p = ptab_init();
+        }
+    }
+
+    (*p)->data = data;
+
+    return *p;
+}
+
+void * ptab_get(ptab *pt, char *prefix)
+{
+    ptab *p = pt;
+    for (size_t i = 0; i < strlen(prefix); i++) {
+        int index = prefix[i] - 'a';
+        if (p->prefix[index] == NULL) {
+            return NULL;
+        }
+        p = p->prefix[index];
+    }
+
+    if (p == NULL) return NULL;
+    return p->data;
 }
--- src/ptab.h
+++ src/ptab.h
@@ -1,14 +1,15 @@
 #ifndef PTAB_H
 #define PTAB_H
 
-#include "user.h"
+#define PTAB_MAX_PREFIX 26
 
 typedef struct ptab {
-    void (* command)(t_user *u, char *data);
-    struct ptab * prefix[26];
+    void *data;
+    struct ptab * prefix[PTAB_MAX_PREFIX];
 } ptab;
 
-ptab * ptab_init(ptab *pt, char *p);
-int ptab_add(ptab *pt, char *prefix, void (* command)(t_user *u, char *data));
+ptab * ptab_init();
+ptab * ptab_add(ptab *pt, char *prefix, void *data);
+void * ptab_get(ptab *pt, char *prefix);
 
 #endif // PTAB_H
